26983cd4112fa1de156aad288eb7580473f0bced
robinsen_global <robinsen_global@126.com>
2018-06-23 17:33:26 +0800

    hello world commit one

---
+++ src/main/java/com/robinsen/git/Apple.java
@@ -0,0 +1,11 @@
+package com.robinsen.git;
+
+/**
+ * 莫莫测试类
+ * 
+ * @author ldfz-wxcz
+ *
+ */
+public class Apple {
+
+}
---
+++ src/test/java/com/robinsen/git/test/MainTest.java
@@ -0,0 +1,5 @@
+package com.robinsen.git.test;
+
+public class MainTest {
+
+}
